159914966513c93cd3aab5062dfd86671ca90ba1
Bonaventure Anselm <bona.anselm@gmail.com>
2023-09-11 21:01:36 +0100

    Registtation and Login templates completed

--- autostock/__init__.py
+++ autostock/__init__.py
@@ -8,10 +8,10 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     from autostock.main.routes import main
-    from autostock.users.routes import users
+    from autostock.mechanics.routes import mechanics
     from autostock.inventory.routes import inventory
     app.register_blueprint(main)
-    app.register_blueprint(users)
+    app.register_blueprint(mechanics)
     app.register_blueprint(inventory)
 
     return app
