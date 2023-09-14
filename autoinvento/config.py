8b541efe779a3277eee95fba9ee57cc07bed4a94
Bonaventure Anselm <bona.anselm@gmail.com>
2023-09-14 11:17:53 +0100

    Updated app secret_key

--- autoinvento/config.py
+++ autoinvento/config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('AUTOSTOCK_SECRET_KEY')
+    SECRET_KEY = os.environ.get('AUTOINVENTO_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///autostock.db'
